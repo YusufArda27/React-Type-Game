2c15609c58c59714247319249846762ba1448de7
yusard <y.ardavardar@gmail.com>
2020-08-30 13:23:35 +0300

    Inıtal

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component {
+    render() {
+      return (
+        <div className="welcome">
+          <h1>Aşşağıdaki kutucağa yazmaya başla</h1>
+          
+        </div>
+      );
+    }
+}
+
+export default Welcome;
